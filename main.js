cb7b2c602a9cd4a7646ca030989c887c7651c0e7
Aleacosta <aleacostagigi@gmail.com>
2023-07-19 02:50:29 -0300

    act while

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-
-
-
 function calculadoraIva(){
-    let valor = prompt("Ingrese el monto de su compra")
-
-    if( valor > 0){
-
-    alert (`El valor sin IVA es: ${100*valor/122}`)
-    }
+    let bucle = true;
     
-    else{
-        alert ("El valor debe ser un numero mayor a 0")
+    while(bucle){
+        
+        let valor =prompt("Ingrese el monto de su compra o escriba 'fin' para finalizar el programa").toUpperCase()
+        
+        
+        if( valor > 0){
+
+        alert (`El valor sin IVA es: ${100*valor/122}`)
+        }
+        else if (valor!= "FIN"){
+            alert ("El valor debe ser un numero mayor a 0")
+            }
+
+        else if(valor = "FIN"){
+        bucle = false
+        alert("El programa finalizo")
+        }
+
+       
     }
 }
+
 
 console.log(calculadoraIva())
 
