c75985cd5c1157ed870ed08728b107f6b68664f5
Aleacosta <aleacostagigi@gmail.com>
2023-07-30 22:56:22 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
 function calculadoraIva(){
-    let bucle = true;
+    let bucleIva = true;
+    let buclePais=true
+    let array =  [{pais: "uruguay", iva: 122},
+                 {pais: "argentina", iva: 121},
+                 {pais: "chile", iva: 119}];
     
-    while(bucle){
-        
+    while(buclePais){
+    let paisIngr= prompt("Ingrese su país:").toLowerCase()
+    
+    let iva;
+    
+    let paisEncontrado= array.find((el)=> el.pais==paisIngr)
+
+    if (paisEncontrado) {
+        iva = paisEncontrado.iva
+        buclePais=false
+      } else {
+        alert("Ese pais no esta disponible");
+      }
+    }  
+
+    while(bucleIva){
+
+
         let valor =prompt("Ingrese el monto de su compra o escriba 'fin' para finalizar el programa").toUpperCase()
         
         
         if( valor > 0){
 
-        alert (`El valor sin IVA es: ${100*valor/122}`)
+        alert (`El valor sin IVA es: ${100*valor/paisEncontrado.iva}`)
         }
         else if (valor!= "FIN"){
             alert ("El valor debe ser un número mayor a 0")
             }
 
         else if(valor = "FIN"){
-        bucle = false
+        bucleIva = false
         alert("El programa finalizó")
         }
 
