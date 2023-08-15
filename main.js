419381b1f0f13580f870e6d49298373c031784ad
Aleacosta <aleacostagigi@gmail.com>
2023-08-14 23:42:04 -0300

    entrega 3 incompleta

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function calculadoraIva(){
+/*function calculadoraIva(){
     let bucleIva = true;
     let buclePais=true
     let array =  [{pais: "uruguay", iva: 122},
                  {pais: "argentina", iva: 121},
                  {pais: "chile", iva: 119}];
-    
+    let paisEncontrado;
     while(buclePais){
     let paisIngr= prompt("Ingrese su país:").toLowerCase()
     
     let iva;
     
-    let paisEncontrado= array.find((el)=> el.pais==paisIngr)
+    paisEncontrado= array.find((el)=> el.pais==paisIngr)
 
     if (paisEncontrado) {
         iva = paisEncontrado.iva
@@ -43,5 +43,57 @@ function calculadoraIva(){
     }
 }
 
-console.log(calculadoraIva())
+console.log(calculadoraIva())*/
 
+
+
+
+function calculadoraIva(){
+ 
+  
+  let iva;
+ 
+  
+  let uruguay = document.getElementById("uruguay")
+  let argentina = document.getElementById("argentina")
+  let chile = document.getElementById("chile")
+  
+  let array =  [{pais: "uruguay", iva: 122},
+                {pais: "argentina", iva: 121},
+                {pais: "chile", iva: 119}];
+  
+  let paisSeleccionado
+  paisSeleccionado = document.querySelector(".radio:checked")
+
+   if (paisSeleccionado = uruguay){
+    iva = 122
+   }
+   else if( paisSeleccionado = argentina){
+    iva = 121
+   }
+   else if(paisSeleccionado = chile){
+    iva = 119
+   }
+
+
+  let monto = document.getElementById("monto")
+  let valor = monto.value
+  let valSin
+
+      if( monto.value > 0){
+
+      alert (`El valor sin IVA es: ${100*valor/iva}`)
+      valSin = 100*valor/iva
+      }
+      else{
+          alert ("El valor debe ser un número mayor a 0")
+          }
+    localStorage.setItem("Monto sin IVA", valSin)
+    localStorage.setItem("Monto con IVA", monto.value)
+}
+
+let boton =document.getElementById("boton")
+let valAnt = document.getElementById("valAnt")
+
+valAnt.addEventListener("click", ()=>{ alert("El valor con IVA anterior es = "+ localStorage.getItem("Monto con IVA") + " y el valor sin IVA es = "+ localStorage.getItem("Monto sin IVA"))})
+boton.addEventListener("click", calculadoraIva)
